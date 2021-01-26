dc4cd3c51eb1d66002dbc3b7d33df1fbe7653a1e
Zheki <47313913+Zheki@users.noreply.github.com>
2021-01-26 20:47:53 +0400

    Add files via upload

---
+++ art_certificate/app.js
@@ -0,0 +1,10 @@
+var button_click = document.getElementById("art_btn");
+
+
+button_click.onclick = function(){ 
+    // document.getElementById("art_btn").style.display = "none";
+    document.getElementById("clicker").style.display = "none";           
+    document.getElementById("certificate").style.display = "block";
+    document.getElementById("done").style.display = "block";  
+    console.log("kurac");
+}
